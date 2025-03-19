68cf67c133162e185adced362ab060affd4bd970
Zainab Wahab <zainabwahab.eth@gmail'com>
2025-03-19 11:15:22 +0000

    Functionality Improvement

--- music-app/src/App.jsx
+++ music-app/src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Api from './components/Api'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- music-app/src/components/Api.jsx
+++ music-app/src/components/Api.jsx
@@ -2,21 +2,17 @@ import styles from "./api.module.css";
 import axios from "axios";
 import React from "react";
 import { useEffect, useState } from "react";
-import playIcon from "../assets/play-icon.svg";
-import play from "../assets/play.png";
-import stop from "../assets/stop.png";
-import pause from "../assets/pause.png";
+import play from "../assets/play.svg";
+import stop from "../assets/stop.svg";
+import pause from "../assets/pause.svg";
 import { Howl } from "howler";
 
 function Api() {
-  fetch("https://robo-music-api.onrender.com/music/my-api")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-
   const API_URL = "https://robo-music-api.onrender.com/music/my-api";
   const [songs, setSongs] = useState([]);
   const [currentSong, setCurrentSong] = useState(null);
   const [sound, setSound] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
     const fetchSongs = function () {
@@ -37,28 +33,78 @@ function Api() {
     setSound(newSound);
     setCurrentSong(song);
     newSound.play();
+    setIsPlaying(true);
+  };
+
+  const stopSong = function () {
+    if (sound) {
+      sound.stop();
+      setIsPlaying(false);
+      setCurrentSong(null);
+    }
+  };
+
+  const pauseSong = function (song) {
+    if (sound) {
+      sound.pause();
+      setIsPlaying(false);
+    }
   };
 
   return (
     <div className={styles.body}>
       <div className={styles.container}>
+        <div className={styles.header}>
+          <h2>Your Music</h2>
+        </div>
+
         <div className={styles.songs}>
           {songs.map((song) => (
             <div key={song.id} className={styles.song}>
               <img
-                className={styles.img}
+                className={styles.imgCntn}
                 src={song.songImage}
                 alt={song.songTitle}
               />
-              {song.songTitle} - {song.artistName}
-              <button onClick={() => playSong(song)}>
+              <div className={styles.detailsCntn}>
+                <h2 className={styles.title}>{song.songTitle.trim()}</h2>
+                <p className={styles.artist}>{song.artistName}</p>
+              </div>
+
+              <button className={styles.button} onClick={() => playSong(song)}>
                 <img src={play} />
               </button>
             </div>
           ))}
-          <div className={styles.nowPlaying}>
-            {currentSong && <h3>Now Playing: {currentSong.songTitle}</h3>}
-          </div>
+        </div>
+        <div>
+          {currentSong ? (
+            <div className={styles.nowPlaying}>
+              <div className={styles.playContent}>
+                <h3>Now Playing</h3>
+                <span>
+                  {currentSong.artistName} - {currentSong.songTitle}{" "}
+                </span>
+              </div>
+              <div className={styles.buttonCntn}>
+                <button
+                  className={styles.button}
+                  onClick={isPlaying ? pauseSong : () => playSong(currentSong)}
+                >
+                  <img src={isPlaying ? pause : play} />
+                </button>
+
+                <button
+                  className={styles.button}
+                  onClick={() => stopSong(currentSong)}
+                >
+                  <img src={stop} />
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
       </div>
     </div>
